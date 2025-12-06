a2b013d6877e3c8ddc442715d1bb4bfbb2a0bb8d
thallys <thallys.rc@gmail.com>
2025-12-06 14:19:06 -0300

    add simple content crud

--- backend/content/views.py
+++ backend/content/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Content
@@ -7,3 +8,7 @@ from .serializers import ContentSerializer
 class ContentViewSet(ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
+    permission_classes = (DjangoModelPermissions,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
